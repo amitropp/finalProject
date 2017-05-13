962748cd728572e8b550a79c7b373fe1eac131b7
Amit Tropp <amitropp@gmail.com>
2017-05-13 21:42:41 +0300

    start of history origin

--- app/src/main/java/com/friends/stay/keepintouch/Contact.java
+++ app/src/main/java/com/friends/stay/keepintouch/Contact.java
@@ -1,85 +1,13 @@
 package com.friends.stay.keepintouch;
 
-import java.util.ArrayList;
-
 /**
- * Created by Amit on 13/05/2017.
+ * Created by Avi on 11/05/2017.
  */
 
 class Contact {
-
-    private String name;
-    private String number;
-    private String nickname;
-    private boolean isWatsApp;
-    private boolean isFacebook;
-    private boolean isSMS;
-    private boolean isCall;
-    private int communicationRate; //in days
-    private ArrayList<Msg> futureMessages;
-    private ArrayList<Msg> historyMessages;
-
-    public Contact(String name, String number, String nickname, boolean isWatsApp,
-                   boolean isFacebook, boolean isSMS, boolean isCall, int communicationRate) {
-        this.name = name;
-        this.number = number;
-        this.nickname = nickname;
-        this.isWatsApp = isWatsApp;
-        this.isFacebook = isFacebook;
-        this.isSMS = isSMS;
-        this.isCall = isCall;
-        this.communicationRate = communicationRate;
-        futureMessages = new ArrayList<Msg>();
-        historyMessages = new ArrayList<Msg>();
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setNumber(String number) {
-        this.number = number;
-    }
-
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
-    }
-
-    public void setCommunicationRate(int communicationRate) {
-        this.communicationRate = communicationRate;
-    }
-
     public String getNumber()
     {
-        //TODO update to real line
-        //return number;
         return "0509223270";
 
-    }
-
-    public void setWatsApp(boolean status){
-        isWatsApp = status;
-    }
-
-    public void setFacebook(boolean status){
-        isFacebook = status;
-    }
-
-    public void setSMS(boolean status){
-        isSMS = status;
-    }
-
-    public void setCall(boolean status){
-        isCall = status;
-    }
-
-    public void addFuture(Msg newMsg){
-        futureMessages.add(newMsg);
-    }
-
-    public void moveToHistory(Msg msgToMove){
-        //TODO  make sure send() dunction od msg call this function
-        futureMessages.remove(msgToMove);
-        historyMessages.add(msgToMove);
     }
 }
--- app/src/main/java/com/friends/stay/keepintouch/MainActivity.java
+++ app/src/main/java/com/friends/stay/keepintouch/MainActivity.java
@@ -1,26 +1,52 @@
 package com.friends.stay.keepintouch;
 
 import android.content.pm.PackageManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageButton;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
     public static final int PERMISSION_REQUEST_CALL = 1 ;
 
-    Call mCall;
+    private Call mNextCall;
+    private ImageButton mAddBtn;
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAddBtn = (ImageButton)findViewById(R.id.ib_add_contact);
 
+        setTabs();
 //        test();
 
     }
 
+    private void setTabs() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
 
 
     private void test()
@@ -41,11 +67,51 @@ public class MainActivity extends AppCompatActivity {
         switch (requestCode) {
             case PERMISSION_REQUEST_CALL:
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    mCall.callNow();
+                    mNextCall.callNow();
                 }
                 break;
         }
     }
 
+
+    //setup the tabs of the page
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new ContactsListFragment(), "CONTACTS");
+//        adapter.addFragment(new MessageViewFragment(), "FUTURE");
+//        adapter.addFragment(new MessageViewFragment(), "HISTORY");
+        adapter.addFragment(new ContactsListFragment(), "FUTURE");
+        adapter.addFragment(new ContactsListFragment(), "HISTORY");
+        viewPager.setAdapter(adapter);
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
 
 }
