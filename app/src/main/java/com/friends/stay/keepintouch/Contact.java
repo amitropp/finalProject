48b72c9856ab21e12a771d834b7069a589371330
Amit Tropp <amitropp@gmail.com>
2017-08-05 17:07:38 +0300

    update user and contact classes

--- app/src/main/java/com/friends/stay/keepintouch/Contact.java
+++ app/src/main/java/com/friends/stay/keepintouch/Contact.java
@@ -1,13 +1,116 @@
 package com.friends.stay.keepintouch;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
 /**
  * Created by Avi on 11/05/2017.
  */
 
 class Contact {
 
-    public String getNumber()
-    {
-        return "0509223270";
+    private String name;
+    private String number;
+    private String nickname;
+    private boolean isWatsApp;
+    private boolean isFacebook;
+    private boolean isSMS;
+    private boolean isCall;
+    private int communicationRate;
+    private ArrayList<Msg> futureMessages;
+    private ArrayList<Msg> historyMessages;
+
+    public Contact(String name,  String number, String nickname, boolean isWatsApp,
+                   boolean isFacebook, boolean isSMS, boolean isCall, int communicationRate) {
+        this.name = name;
+        this.number = number;
+        this.nickname = nickname;
+        this.isWatsApp = isWatsApp;
+        this.isFacebook = isFacebook;
+        this.isSMS = isSMS;
+        this.isCall = isCall;
+        this.communicationRate = communicationRate;
+        futureMessages = new ArrayList<Msg>();
+        historyMessages = new ArrayList<Msg>();
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public boolean isWatsApp() {
+        return isWatsApp;
+    }
+
+    public void setWatsApp() {
+        isWatsApp = !isWatsApp;
+    }
+
+    public boolean isFacebook() {
+        return isFacebook;
+    }
+
+    public void setFacebook() {
+        isFacebook = !isFacebook;
+    }
+
+    public boolean isSMS() {
+        return isSMS;
+    }
+
+    public void setSMS() {
+        isSMS = !isSMS;
+    }
+
+    public boolean isCall() {
+        return isCall;
+    }
+
+    public void setCall() {
+        isCall = !isCall;
+    }
+
+    public int getCommunicationRate() {
+        return communicationRate;
+    }
+
+    public void setCommunicationRate(int communicationRate) {
+        this.communicationRate = communicationRate;
+    }
+
+
+    public void addFutureMessages(Msg newMessages) {
+        futureMessages.add(newMessages);
+    }
+
+    public ArrayList<Msg> getHistoryMessages() {
+        return historyMessages;
+    }
+
+    public void addHistoryMessages(Msg newMessages) {
+        historyMessages.add(newMessages);
+    }
+
+
 }
