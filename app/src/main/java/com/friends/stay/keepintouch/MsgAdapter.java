34adf166265a70b3b795860055e03eaac54bb68a
Amit Tropp <amitropp@gmail.com>
2017-08-18 13:20:24 +0300

    fix error

--- app/src/main/java/com/friends/stay/keepintouch/MsgAdapter.java
+++ app/src/main/java/com/friends/stay/keepintouch/MsgAdapter.java
@@ -1,8 +1,42 @@
 package com.friends.stay.keepintouch;
 
+import android.widget.ArrayAdapter;
+
 /**
  * Created by amitropp on 18/08/2017.
  */
 
-public class MsgAdapter {
-}
+public class MsgAdapter{ // extends ArrayAdapter<Msg> {
+
+//    private static class ViewHolder {gir
+//        private TextView itemView;
+//    }
+//
+//    public MyClassAdapter(Context context, int textViewResourceId, ArrayList<MyClass> items) {
+//        super(context, textViewResourceId, items);
+//    }
+//
+//    public View getView(int position, View convertView, ViewGroup parent) {
+//
+//        if (convertView == null) {
+//            convertView = LayoutInflater.from(this.getContext())
+//                    .inflate(R.layout.listview_association, parent, false);
+//
+//            viewHolder = new ViewHolder();
+//            viewHolder.itemView = (TextView) convertView.findViewById(R.id.ItemView);
+//
+//            convertView.setTag(viewHolder);
+//        } else {
+//            viewHolder = (ViewHolder) convertView.getTag();
+//        }
+//
+//        MyClass item = getItem(position);
+//        if (item!= null) {
+//            // My layout has only one TextView
+//            // do whatever you want with your string and long
+//            viewHolder.itemView.setText(String.format("%s %d", item.reason, item.long_val));
+//        }
+//
+//        return convertView;
+//    }
+ }
