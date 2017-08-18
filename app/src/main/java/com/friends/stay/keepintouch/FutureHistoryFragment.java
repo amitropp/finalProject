99b82c5b5123e529335b2980ddc5a58803a58ea4
avihnd <avi.hendler@mail.huji.ac.il>
2017-08-18 13:18:50 +0300

    new futureHistoryFragment

---
+++ app/src/main/java/com/friends/stay/keepintouch/FutureHistoryFragment.java
@@ -0,0 +1,62 @@
+package com.friends.stay.keepintouch;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+
+/**
+ * Future and History display of messages
+ */
+public class FutureHistoryFragment extends Fragment {
+    private View myView = null;
+    private boolean isFuture = false;
+    private ImageButton mAddMessageBtn;
+
+    public static FutureHistoryFragment newInstance(boolean isFuture) {
+        FutureHistoryFragment  newFrag = new FutureHistoryFragment();
+        Bundle args = new Bundle();
+        args.putBoolean("isFuture", isFuture);
+        newFrag.setArguments(args);
+        return newFrag;
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle args = getArguments();
+        if (args != null) {
+            boolean isFutureFrag = args.getBoolean("isFuture", false);
+            if (isFutureFrag) {
+                this.isFuture = true;
+            }
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        //create view only once per instance
+        if (myView == null)
+        {
+            // Inflate the layout for this fragment
+            View view = inflater.inflate(R.layout.fragment_future_history, container, false);
+            myView = view;
+            mAddMessageBtn = (ImageButton)view.findViewById(R.id.ib_add_message);
+//            MainActivity mainActivity = (MainActivity)getActivity();
+
+
+
+            return view;
+        }
+        return myView;
+
+    }
+
+}
