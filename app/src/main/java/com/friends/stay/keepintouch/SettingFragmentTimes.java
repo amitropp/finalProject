9e8426a7eb18a89e13f6d61b0302fe1070f5e72f
Amit Tropp <amitropp@gmail.com>
2017-08-25 15:31:20 +0300

    SettingFragmentTimes woek with save

--- app/src/main/java/com/friends/stay/keepintouch/SettingFragmentTimes.java
+++ app/src/main/java/com/friends/stay/keepintouch/SettingFragmentTimes.java
@@ -46,73 +46,173 @@ public class SettingFragmentTimes extends Fragment {
     //Sunday
     CheckBox cvSundayMorning;
     boolean isSundayMorning;
+    CheckBox cvSundayNoon;
     boolean isSundayNoon;
+    CheckBox cvSundayEvening;
     boolean isSundayEvening;
 
     //Monday
+    CheckBox cvMondayMorning;
     boolean isMondayMorning;
+    CheckBox cvMondayNoon;
     boolean isMondayNoon;
+    CheckBox cvMondayEvening;
     boolean isMondayEvening;
 
     //Tuesday
+    CheckBox cvTuesdayMorning;
     boolean isTuesdayMorning;
+    CheckBox cvTuesdayNoon;
     boolean isTuesdayNoon;
+    CheckBox cvTuesdayEvening;
     boolean isTuesdayEvening;
 
     //Wednesday
+    CheckBox cvWednesdayMorning;
     boolean isWednesdayMorning;
+    CheckBox cvWednesdayNoon;
     boolean isWednesdayNoon;
+    CheckBox cvWednesdayEvening;
     boolean isWednesdayEvening;
 
     //Thursday
+    CheckBox cvThursdayMorning;
     boolean isThursdayMorning;
+    CheckBox cvThursdayNoon;
     boolean isThursdayNoon;
+    CheckBox cvThursdayEvening;
     boolean isThursdayEvening;
 
     //Friday
+    CheckBox cvFridayMorning;
     boolean isFridayMorning;
+    CheckBox cvFridayNoon;
     boolean isFridayNoon;
+    CheckBox cvFridayEvening;
     boolean isFridayEvening;
 
     //Saturday
+    CheckBox cvSaturdayMorning;
     boolean isSaturdayMorning;
+    CheckBox cvSaturdayNoon;
     boolean isSaturdayNoon;
+    CheckBox cvSaturdayEvening;
     boolean isSaturdayEvening;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fregment_setting_times, container, false);
-//        mDoneBtn = (Button)view.findViewById(R.id.btn_done);
         preferences = PreferenceManager.getDefaultSharedPreferences(getContext().getApplicationContext());
         thisView = view;
-        cvSundayMorning = (CheckBox)thisView.findViewById(R.id.sunday_morning);
+
+        //create all check boxs
+        //Sunday
+        cvSundayMorning = (CheckBox) thisView.findViewById(R.id.sunday_morning);
+        cvSundayNoon = (CheckBox) thisView.findViewById(R.id.sunday_noon);
+        cvSundayEvening = (CheckBox) thisView.findViewById(R.id.sunday_evening);
+
+        //Monday
+        cvMondayMorning = (CheckBox) thisView.findViewById(R.id.monday_morning);
+        cvMondayNoon = (CheckBox) thisView.findViewById(R.id.monday_noon);
+        cvMondayEvening = (CheckBox) thisView.findViewById(R.id.monday_evening);
+
+        //Tuesday
+        cvTuesdayMorning = (CheckBox) thisView.findViewById(R.id.tuesday_morning);
+        cvTuesdayNoon = (CheckBox) thisView.findViewById(R.id.tuesday_noon);
+        cvTuesdayEvening = (CheckBox) thisView.findViewById(R.id.tuesday_evening);
+
+        //Wednesday
+        cvWednesdayMorning = (CheckBox) thisView.findViewById(R.id.wednesday_morning);
+        cvWednesdayNoon = (CheckBox) thisView.findViewById(R.id.wednesday_noon);
+        cvWednesdayEvening = (CheckBox) thisView.findViewById(R.id.wednesday_evening);
+
+        //Thursday
+        cvThursdayMorning = (CheckBox) thisView.findViewById(R.id.thursday_morning);
+        cvThursdayNoon = (CheckBox) thisView.findViewById(R.id.thursday_noon);
+        cvThursdayEvening = (CheckBox) thisView.findViewById(R.id.thursday_evening);
+
+        //Friday
+        cvFridayMorning = (CheckBox) thisView.findViewById(R.id.friday_morning);
+        cvFridayNoon = (CheckBox) thisView.findViewById(R.id.friday_noon);
+        cvFridayEvening = (CheckBox) thisView.findViewById(R.id.friday_evening);
+
+        //Saturday
+        cvSaturdayMorning = (CheckBox) thisView.findViewById(R.id.saturday_morning);
+        cvSaturdayNoon = (CheckBox) thisView.findViewById(R.id.saturday_noon);
+        cvSaturdayEvening = (CheckBox) thisView.findViewById(R.id.saturday_evening);
+
         setTimes();
         return view;
 
     }
 
     @Override
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-//        outState.putBoolean("isSundayMorning", isSundayMorning);
-        Log.d("save as= ", String.valueOf(isSundayMorning));
-        preferences.edit().putBoolean("isSundayMorning", isSundayMorning).commit();
-    }
-
-    @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-//        isSundayMorning = savedInstanceState.getBoolean("isSundayMorning");
+
+        //Sunday
         isSundayMorning = preferences.getBoolean("isSundayMorning", false);
         Log.d("isSundayMorning = ", String.valueOf(isSundayMorning));
         cvSundayMorning.setChecked(isSundayMorning);
-//        if (isSundayMorning) {
-//            cvSundayMorning.setChecked(true);
-//        }
+        isSundayNoon = preferences.getBoolean("isSundayNoon", false);
+        Log.d("isSundayNoon = ", String.valueOf(isSundayNoon));
+        cvSundayNoon.setChecked(isSundayNoon);
+        isSundayEvening = preferences.getBoolean("isSundayEvening", false);
+        cvSundayEvening.setChecked(isSundayEvening);
+
+        //Monday
+        isMondayMorning = preferences.getBoolean("isMondayMorning", false);
+        cvMondayMorning.setChecked(isMondayMorning);
+        isMondayNoon = preferences.getBoolean("isMondayNoon", false);
+        cvMondayNoon.setChecked(isMondayNoon);
+        isMondayEvening = preferences.getBoolean("isMondayEvening", false);
+        cvMondayEvening.setChecked(isMondayEvening);
+
+        //Tuesday
+        isTuesdayMorning = preferences.getBoolean("isTuesdayMorning", false);
+        cvTuesdayMorning.setChecked(isTuesdayMorning);
+        isTuesdayNoon = preferences.getBoolean("isTuesdayNoon", false);
+        cvTuesdayNoon.setChecked(isTuesdayNoon);
+        isTuesdayEvening = preferences.getBoolean("isTuesdayEvening", false);
+        cvTuesdayEvening.setChecked(isTuesdayEvening);
+
+        //Wednesday
+        isWednesdayMorning = preferences.getBoolean("isWednesdayMorning", false);
+        cvWednesdayMorning.setChecked(isWednesdayMorning);
+        isWednesdayNoon = preferences.getBoolean("isWednesdayNoon", false);
+        cvWednesdayNoon.setChecked(isWednesdayNoon);
+        isWednesdayEvening = preferences.getBoolean("isWednesdayEvening", false);
+        cvWednesdayEvening.setChecked(isWednesdayEvening);
+
+        //Thursday
+        isThursdayMorning = preferences.getBoolean("isThursdayMorning", false);
+        cvThursdayMorning.setChecked(isThursdayMorning);
+        isThursdayNoon = preferences.getBoolean("isThursdayNoon", false);
+        cvThursdayNoon.setChecked(isThursdayNoon);
+        isThursdayEvening = preferences.getBoolean("isThursdayEvening", false);
+        cvThursdayEvening.setChecked(isThursdayEvening);
+
+        //Friday
+        isFridayMorning = preferences.getBoolean("isFridayMorning", false);
+        cvFridayMorning.setChecked(isFridayMorning);
+        isFridayNoon = preferences.getBoolean("isFridayNoon", false);
+        cvFridayNoon.setChecked(isFridayNoon);
+        isFridayEvening = preferences.getBoolean("isFridayEvening", false);
+        cvFridayEvening.setChecked(isFridayEvening);
+
+        //Saturday
+        isSaturdayMorning = preferences.getBoolean("isSaturdayMorning", false);
+        cvSaturdayMorning.setChecked(isSaturdayMorning);
+        isSaturdayNoon = preferences.getBoolean("isSaturdayNoon", false);
+        cvSaturdayNoon.setChecked(isSaturdayNoon);
+        isSaturdayEvening = preferences.getBoolean("isSaturdayEvening", false);
+        cvSaturdayEvening.setChecked(isSaturdayEvening);
+
     }
 
     public void setTimes(){
+        Log.d("", "setTimes");
         mDoneBtn = (Button) thisView.findViewById(R.id.btn_done);
         mDoneBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -124,63 +224,80 @@ public class SettingFragmentTimes extends Fragment {
 
     // wait for the user to click the done button
     private void _setDoneListener() {
+        Log.d("", "_setDoneListener");
         //get times - Sunday
-//        CheckBox cvSundayMorning = (CheckBox)thisView.findViewById(R.id.sunday_morning);
         isSundayMorning = cvSundayMorning.isChecked();
-        preferences.edit().putBoolean("isSundayMorning", isSundayMorning).commit();
-        Log.d("in done= ", String.valueOf(isSundayMorning));
-        CheckBox cvSundayNoon = (CheckBox)thisView.findViewById(R.id.sunday_noon);
         isSundayNoon = cvSundayNoon.isChecked();
-        CheckBox cvSundayEvening = (CheckBox)thisView.findViewById(R.id.sunday_evening);
         isSundayEvening = cvSundayEvening.isChecked();
 
         //get times - Monday
-        CheckBox cvMondayMorning = (CheckBox)thisView.findViewById(R.id.monday_morning);
         isMondayMorning = cvMondayMorning.isChecked();
-        CheckBox cvMondayNoon = (CheckBox)thisView.findViewById(R.id.monday_noon);
         isMondayNoon = cvMondayNoon.isChecked();
-        CheckBox cvMondayEvening = (CheckBox)thisView.findViewById(R.id.monday_evening);
         isMondayEvening = cvMondayEvening.isChecked();
 
         //get times - Tuesday
-        CheckBox cvTuesdayMorning = (CheckBox)thisView.findViewById(R.id.tuesday_morning);
         isTuesdayMorning = cvTuesdayMorning.isChecked();
-        CheckBox cvTuesdayNoon = (CheckBox)thisView.findViewById(R.id.tuesday_noon);
         isTuesdayNoon = cvTuesdayNoon.isChecked();
-        CheckBox cvTuesdayEvening = (CheckBox)thisView.findViewById(R.id.tuesday_evening);
         isTuesdayEvening = cvTuesdayEvening.isChecked();
 
         //get times - Wednesday
-        CheckBox cvWednesdayMorning = (CheckBox)thisView.findViewById(R.id.wednesday_morning);
         isWednesdayMorning = cvWednesdayMorning.isChecked();
-        CheckBox cvWednesdayNoon = (CheckBox)thisView.findViewById(R.id.wednesday_noon);
         isWednesdayNoon = cvWednesdayNoon.isChecked();
-        CheckBox cvWednesdayEvening = (CheckBox)thisView.findViewById(R.id.wednesday_evening);
         isWednesdayEvening = cvWednesdayEvening.isChecked();
 
         //get times - Thursday
-        CheckBox cvThursdayMorning = (CheckBox)thisView.findViewById(R.id.thursday_morning);
         isThursdayMorning = cvThursdayMorning.isChecked();
-        CheckBox cvThursdayNoon = (CheckBox)thisView.findViewById(R.id.thursday_noon);
         isThursdayNoon = cvThursdayNoon.isChecked();
-        CheckBox cvThursdayEvening = (CheckBox)thisView.findViewById(R.id.thursday_evening);
         isThursdayEvening = cvThursdayEvening.isChecked();
 
         //get times - Friday
-        CheckBox cvFridayMorning = (CheckBox)thisView.findViewById(R.id.friday_morning);
         isFridayMorning = cvFridayMorning.isChecked();
-        CheckBox cvFridayNoon = (CheckBox)thisView.findViewById(R.id.friday_noon);
         isFridayNoon = cvFridayNoon.isChecked();
-        CheckBox cvFridayEvening = (CheckBox)thisView.findViewById(R.id.friday_evening);
         isFridayEvening = cvFridayEvening.isChecked();
 
         //get times - Saturday
-        CheckBox cvSaturdayMorning = (CheckBox)thisView.findViewById(R.id.saturday_morning);
         isSaturdayMorning = cvSaturdayMorning.isChecked();
-        CheckBox cvSaturdayNoon = (CheckBox)thisView.findViewById(R.id.saturday_noon);
         isSaturdayNoon = cvSaturdayNoon.isChecked();
-        CheckBox cvSaturdayEvening = (CheckBox)thisView.findViewById(R.id.saturday_evening);
         isSaturdayEvening = cvSaturdayEvening.isChecked();
+
+
+        //save times
+        //Sunday
+        preferences.edit().putBoolean("isSundayMorning", isSundayMorning).commit();
+        Log.d("save isSundayMorning = ", String.valueOf(isSundayMorning));
+        preferences.edit().putBoolean("isSundayNoon", isSundayNoon).commit();
+        Log.d("save isSundayNoon = ", String.valueOf(isSundayNoon));
+        preferences.edit().putBoolean("isSundayEvening", isSundayEvening).commit();
+
+        //Monday
+        preferences.edit().putBoolean("isMondayMorning", isMondayMorning).commit();
+        preferences.edit().putBoolean("isMondayNoon", isMondayNoon).commit();
+        preferences.edit().putBoolean("isMondayEvening", isMondayEvening).commit();
+
+        //Tuesday
+        preferences.edit().putBoolean("isTuesdayMorning", isTuesdayMorning).commit();
+        preferences.edit().putBoolean("isTuesdayNoon", isTuesdayNoon).commit();
+        preferences.edit().putBoolean("isTuesdayEvening", isTuesdayEvening).commit();
+
+        //Wednesday
+        preferences.edit().putBoolean("isWednesdayMorning", isWednesdayMorning).commit();
+        preferences.edit().putBoolean("isWednesdayNoon", isWednesdayNoon).commit();
+        preferences.edit().putBoolean("isWednesdayEvening", isWednesdayEvening).commit();
+
+        //Thursday
+        preferences.edit().putBoolean("isThursdayMorning", isThursdayMorning).commit();
+        preferences.edit().putBoolean("isThursdayNoon", isThursdayNoon).commit();
+        preferences.edit().putBoolean("isThursdayEvening", isThursdayEvening).commit();
+
+        //Friday
+        preferences.edit().putBoolean("isFridayMorning", isFridayMorning).commit();
+        preferences.edit().putBoolean("isFridayNoon", isFridayNoon).commit();
+        preferences.edit().putBoolean("isFridayEvening", isFridayEvening).commit();
+
+        //Saturday
+        preferences.edit().putBoolean("isSaturdayMorning", isSaturdayMorning).commit();
+        preferences.edit().putBoolean("isSaturdayNoon", isSaturdayNoon).commit();
+        preferences.edit().putBoolean("isSaturdayEvening", isSaturdayEvening).commit();
 
         //reset to all available times before updating
         MainActivity.getUser().clearAvailableTimes();
